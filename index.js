554d3a4ec1560f691fb8887fc1bfd74b155872c6
vdemedes <vdemedes@gmail.com>
2016-10-11 20:02:46 +0300

    es5ify code

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 'use strict';
 
-/**
- * Expose format-object
- */
-
-module.exports = format;
-
-
-/**
- * Format string with keys and values
- */
-
-function format (str, params) {
-  let keys = Object.keys(params);
+function formatObject (str, params) {
+  var keys = Object.keys(params);
 
   keys.forEach(function (key) {
-    let value = params[key];
+    var value = params[key];
 
     str = str.replace(':' + key, value);
   });
 
   return str;
 }
+
+module.exports = formatObject;
